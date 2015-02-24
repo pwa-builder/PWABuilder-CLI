c411b2a7c2648a4947215cc19b960843e6dec72e
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2015-02-24 19:48:31 -0300

    # [Manifest] Added test namespaces

--- test/tools.js
+++ test/tools.js
@@ -30,304 +30,305 @@ var outputFiles = {
   validManifestPath: path.join(assetsDirectory, 'output-manifest.json')
 };
 
+describe('Tools', function () {
+  describe('getManifestFromFile()', function () {
+    it('Invalid path should return an Error in callback.', function(done) {
+      tools.getManifestFromFile(inputFiles.notExistingFile, function(err){
+        should.exist(err);
+        done();
+      });
+    });
 
-describe('getManifestFromFile()', function () {
-  it('Invalid path should return an Error in callback.', function(done) {
-    tools.getManifestFromFile(inputFiles.notExistingFile, function(err){
-      should.exist(err);
-      done();
+    it('Invalid json format should return an Error in callback.', function(done) {
+      tools.getManifestFromFile(inputFiles.invalidManifest, function(err){
+        should.exist(err);
+        err.should.have.property('message', 'Invalid manifest format');
+        done();
+      });
+    });
+
+    it('Invalid manifest format should return an Error in callback.');
+
+    it('Valid manifest file should return a manifest info object in callback.', function(done) {
+      tools.getManifestFromFile(inputFiles.validManifest, function(err, manifestInfo){
+        should.not.exist(err);
+        should.exist(manifestInfo);
+        manifestInfo.should.have.property('content');
+        done();
+      });
     });
   });
 
-  it('Invalid json format should return an Error in callback.', function(done) {
-    tools.getManifestFromFile(inputFiles.invalidManifest, function(err){
-      should.exist(err);
-      err.should.have.property('message', 'Invalid manifest format');
-      done();
+  describe('writeToFile()', function () {
+    it('if manifestInfo is undefined, it should should return an Error in callback.', function(done) {
+      tools.writeToFile(undefined, outputFiles.invalidManifestPath, function(err){
+        should.exist(err);
+        err.should.have.property('message', 'Manifest content is empty or not initialized.');
+        done();
+      });
+    });
+
+    it('if manifestInfo does not contains content property, it should should return an Error in callback.', function(done) {
+      tools.writeToFile({ key: 'value' }, outputFiles.invalidManifestPath, function(err){
+        should.exist(err);
+        err.should.have.property('message', 'Manifest content is empty or not initialized.');
+        done();
+      });
+    });
+
+    it('Write errors should return be returned as an Error in callback.', function(done) {
+      tools.writeToFile({ content: { 'start_url': 'url' } }, outputFiles.invalidManifestPath, function(err){
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('Should write only the manifest information object content in file.', function(done) {
+      tools.writeToFile({ content: { 'start_url': 'url' } }, outputFiles.validManifestPath, function(err){
+        should.not.exist(err);
+        done();
+      });
+    });
+
+    after(function() {
+      // runs after all tests in this block
+
+      fs.exists(outputFiles.validManifestPath, function (exists) {
+        if(exists) {
+          fs.unlink(outputFiles.validManifestPath, function (err) {
+            if (err) {
+              throw err;
+            }
+          });
+        }
+      });
     });
   });
 
-  it('Invalid manifest format should return an Error in callback.');
-
-  it('Valid manifest file should return a manifest info object in callback.', function(done) {
-    tools.getManifestFromFile(inputFiles.validManifest, function(err, manifestInfo){
-      should.not.exist(err);
-      should.exist(manifestInfo);
-      manifestInfo.should.have.property('content');
-      done();
+  describe('getManifestUrlFromSite()', function () {
+    before(function () {
+      server.listen(8042);
     });
-  });
-});
 
-describe('writeToFile()', function () {
-  it('if manifestInfo is undefined, it should should return an Error in callback.', function(done) {
-    tools.writeToFile(undefined, outputFiles.invalidManifestPath, function(err){
-      should.exist(err);
-      err.should.have.property('message', 'Manifest content is empty or not initialized.');
-      done();
+    it('Invalid url should return an Error in callback.', function(done) {
+      responseFunction = function() {
+        should.fail('This function should not be called in this test');
+      };
+
+      tools.getManifestUrlFromSite('invalid url', function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'Failed to retrieve manifest from site.');
+        done();
+      });
     });
-  });
 
-  it('if manifestInfo does not contains content property, it should should return an Error in callback.', function(done) {
-    tools.writeToFile({ key: 'value' }, outputFiles.invalidManifestPath, function(err){
-      should.exist(err);
-      err.should.have.property('message', 'Manifest content is empty or not initialized.');
-      done();
+    it('Valid url but returning 404 should return an Error in callback.', function(done) {
+      responseFunction = function(req, res) {
+        res.writeHead(404);
+        res.end();
+      };
+
+      tools.getManifestUrlFromSite('http://localhost:8042/notfound', function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'Failed to retrieve manifest from site.');
+        done();
+      });
     });
-  });
 
-  it('Write errors should return be returned as an Error in callback.', function(done) {
-    tools.writeToFile({ content: { 'start_url': 'url' } }, outputFiles.invalidManifestPath, function(err){
-      should.exist(err);
-      done();
+    it('Should retrieve undefined from a site that does not contains the manifest tag.', function(done) {
+      responseFunction = function(req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        res.end('<!doctype>' +
+                '<html>' +
+                  '<head>' +
+                    '<title>test</title>' +
+                  '</head>' +
+                  '<body></body>' +
+                '</html>');
+      };
+
+      tools.getManifestUrlFromSite('http://localhost:8042/urlWithoutManifestTag', function(err, manifestUrl) {
+        should.not.exist(err);
+        should.not.exist(manifestUrl);
+        done();
+      });
     });
-  });
 
-  it('Should write only the manifest information object content in file.', function(done) {
-    tools.writeToFile({ content: { 'start_url': 'url' } }, outputFiles.validManifestPath, function(err){
-      should.not.exist(err);
-      done();
+    it('Should retrieve the manifest url from a site that contains the manifest tag with relative manifest url.', function(done) {
+      responseFunction = function(req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        res.end('<!doctype>' +
+                '<html>' +
+                  '<head>' +
+                    '<title>test</title>' +
+                    '<link rel="manifest" href="manifest.json">' +
+                  '</head>' +
+                  '<body></body>' +
+                '</html>');
+      };
+
+      tools.getManifestUrlFromSite('http://localhost:8042/urlWithManifestTag', function(err, manifestUrl) {
+        should.not.exist(err);
+        should.exist(manifestUrl);
+        manifestUrl.should.be.equal('http://localhost:8042/manifest.json');
+        done();
+      });
     });
-  });
 
-  after(function() {
-    // runs after all tests in this block
+    it('Should retrieve the manifest url from a site that contains the manifest tag with absolute manifest url.', function(done) {
+      responseFunction = function(req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    fs.exists(outputFiles.validManifestPath, function (exists) {
-      if(exists) {
-        fs.unlink(outputFiles.validManifestPath, function (err) {
-          if (err) {
-            throw err;
-          }
-        });
-      }
+        res.end('<!doctype>' +
+                '<html>' +
+                  '<head>' +
+                    '<title>test</title>' +
+                    '<link rel="manifest" href="http://www.contoso.com/manifest.json">' +
+                  '</head>' +
+                  '<body></body>' +
+                '</html>');
+      };
+
+      tools.getManifestUrlFromSite('http://localhost:8042/urlWithManifestTag', function(err, manifestUrl) {
+        should.not.exist(err);
+        should.exist(manifestUrl);
+        manifestUrl.should.be.equal('http://www.contoso.com/manifest.json');
+        done();
+      });
     });
-  });
-});
 
-describe('getManifestUrlFromSite()', function () {
-  before(function () {
-    server.listen(8042);
-  });
-
-  it('Invalid url should return an Error in callback.', function(done) {
-    responseFunction = function() {
-      should.fail('This function should not be called in this test');
-    };
-
-    tools.getManifestUrlFromSite('invalid url', function(err) {
-      should.exist(err);
-      err.should.have.property('message', 'Failed to retrieve manifest from site.');
-      done();
+    afterEach(function () {
+      responseFunction = undefined;
     });
-  });
 
-  it('Valid url but returning 404 should return an Error in callback.', function(done) {
-    responseFunction = function(req, res) {
-      res.writeHead(404);
-      res.end();
-    };
-
-    tools.getManifestUrlFromSite('http://localhost:8042/notfound', function(err) {
-      should.exist(err);
-      err.should.have.property('message', 'Failed to retrieve manifest from site.');
-      done();
-    });
-  });
-
-  it('Should retrieve undefined from a site that does not contains the manifest tag.', function(done) {
-    responseFunction = function(req, res) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-
-      res.end('<!doctype>' +
-              '<html>' +
-                '<head>' +
-                  '<title>test</title>' +
-                '</head>' +
-                '<body></body>' +
-              '</html>');
-    };
-
-    tools.getManifestUrlFromSite('http://localhost:8042/urlWithoutManifestTag', function(err, manifestUrl) {
-      should.not.exist(err);
-      should.not.exist(manifestUrl);
-      done();
+    after(function () {
+      server.close();
     });
   });
 
-  it('Should retrieve the manifest url from a site that contains the manifest tag with relative manifest url.', function(done) {
-    responseFunction = function(req, res) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+  describe('downloadManifestFromUrl()', function () {
+    before(function () {
+      server.listen(8042);
+    });
 
-      res.end('<!doctype>' +
-              '<html>' +
-                '<head>' +
-                  '<title>test</title>' +
-                  '<link rel="manifest" href="manifest.json">' +
-                '</head>' +
-                '<body></body>' +
-              '</html>');
-    };
+    it('Invalid url should return an Error in callback.', function(done) {
+      responseFunction = function() {
+        should.fail('This function should not be called in this test');
+      };
 
-    tools.getManifestUrlFromSite('http://localhost:8042/urlWithManifestTag', function(err, manifestUrl) {
-      should.not.exist(err);
-      should.exist(manifestUrl);
-      manifestUrl.should.be.equal('http://localhost:8042/manifest.json');
-      done();
+      tools.downloadManifestFromUrl('invalid url', function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'Failed to download manifest data.');
+        done();
+      });
+    });
+
+    it('Valid url but returning 404 should return an Error in callback.', function(done) {
+      responseFunction = function(req, res) {
+        res.writeHead(404);
+        res.end();
+      };
+
+      tools.downloadManifestFromUrl('http://localhost:8042/notfound', function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'Failed to download manifest data.');
+        done();
+      });
+    });
+
+    it('Valid url but invalid manifest format should return an Error in callback.', function(done) {
+      responseFunction = function(req, res) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        res.end('invalid json');
+      };
+
+      tools.downloadManifestFromUrl('http://localhost:8042/invalidJson', function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'Invalid manifest format.');
+        done();
+      });
+    });
+
+    it('Should retrieve the manifest info object from a site.', function(done) {
+      responseFunction = function(req, res) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        res.end(JSON.stringify({'start_url': 'http://www.contoso.com/'}));
+      };
+
+      tools.downloadManifestFromUrl('http://localhost:8042/validManifest.json', function(err, manifestInfo) {
+        should.not.exist(err);
+        should.exist(manifestInfo);
+        manifestInfo.should.have.properties('content', 'format');
+        done();
+      });
+    });
+
+    afterEach(function () {
+      responseFunction = undefined;
+    });
+
+    after(function () {
+      server.close();
     });
   });
 
-  it('Should retrieve the manifest url from a site that contains the manifest tag with absolute manifest url.', function(done) {
-    responseFunction = function(req, res) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+  describe('getManifestFromSite()', function () {
+    before(function () {
+      server.listen(8042);
+    });
 
-      res.end('<!doctype>' +
-              '<html>' +
-                '<head>' +
-                  '<title>test</title>' +
-                  '<link rel="manifest" href="http://www.contoso.com/manifest.json">' +
-                '</head>' +
-                '<body></body>' +
-              '</html>');
-    };
+    it('Invalid url should return an Error in callback.', function(done) {
+      responseFunction = function() {
+        should.fail('This function should not be called in this test');
+      };
 
-    tools.getManifestUrlFromSite('http://localhost:8042/urlWithManifestTag', function(err, manifestUrl) {
-      should.not.exist(err);
-      should.exist(manifestUrl);
-      manifestUrl.should.be.equal('http://www.contoso.com/manifest.json');
-      done();
+      tools.getManifestFromSite('invalid url', function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'Failed to retrieve manifest from site.');
+        done();
+      });
+    });
+
+    it('Valid url but returning 404 should return an Error in callback.', function(done) {
+      responseFunction = function(req, res) {
+        res.writeHead(404);
+        res.end();
+      };
+
+      tools.getManifestFromSite('http://localhost:8042/notfound', function(err) {
+        should.exist(err);
+        err.should.have.property('message', 'Failed to retrieve manifest from site.');
+        done();
+      });
+    });
+
+    it('Should retrieve the manifest info object from a site that contains the manifest tag.');
+
+    it('Should create the manifest info object from a site that does not contains the manifest tag.');
+
+    //responseFunction = function(req, res) {
+    //  var url_parts = url.parse(req.url);
+    //  var route = url_parts.pathname;
+    //});
+
+    afterEach(function () {
+      responseFunction = undefined;
+    });
+
+    after(function () {
+      server.close();
     });
   });
 
-  afterEach(function () {
-    responseFunction = undefined;
+  describe('convertTo()', function () {
+    it('Convert from W3C to chromeOS.');
+    it('Convert from W3C to W3C.');
+    it('Convert from chromeOS to W3C.');
+    it('Convert from invalid to W3C.');
+    it('Convert from W3C to invalid.');
   });
-
-  after(function () {
-    server.close();
-  });
-});
-
-describe('downloadManifestFromUrl()', function () {
-  before(function () {
-    server.listen(8042);
-  });
-
-  it('Invalid url should return an Error in callback.', function(done) {
-    responseFunction = function() {
-      should.fail('This function should not be called in this test');
-    };
-
-    tools.downloadManifestFromUrl('invalid url', function(err) {
-      should.exist(err);
-      err.should.have.property('message', 'Failed to download manifest data.');
-      done();
-    });
-  });
-
-  it('Valid url but returning 404 should return an Error in callback.', function(done) {
-    responseFunction = function(req, res) {
-      res.writeHead(404);
-      res.end();
-    };
-
-    tools.downloadManifestFromUrl('http://localhost:8042/notfound', function(err) {
-      should.exist(err);
-      err.should.have.property('message', 'Failed to download manifest data.');
-      done();
-    });
-  });
-
-  it('Valid url but invalid manifest format should return an Error in callback.', function(done) {
-    responseFunction = function(req, res) {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-
-      res.end('invalid json');
-    };
-
-    tools.downloadManifestFromUrl('http://localhost:8042/invalidJson', function(err) {
-      should.exist(err);
-      err.should.have.property('message', 'Invalid manifest format.');
-      done();
-    });
-  });
-
-  it('Should retrieve the manifest info object from a site.', function(done) {
-    responseFunction = function(req, res) {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-
-      res.end(JSON.stringify({'start_url': 'http://www.contoso.com/'}));
-    };
-
-    tools.downloadManifestFromUrl('http://localhost:8042/validManifest.json', function(err, manifestInfo) {
-      should.not.exist(err);
-      should.exist(manifestInfo);
-      manifestInfo.should.have.properties('content', 'format');
-      done();
-    });
-  });
-
-  afterEach(function () {
-    responseFunction = undefined;
-  });
-
-  after(function () {
-    server.close();
-  });
-});
-
-describe('getManifestFromSite()', function () {
-  before(function () {
-    server.listen(8042);
-  });
-
-  it('Invalid url should return an Error in callback.', function(done) {
-    responseFunction = function() {
-      should.fail('This function should not be called in this test');
-    };
-
-    tools.getManifestFromSite('invalid url', function(err) {
-      should.exist(err);
-      err.should.have.property('message', 'Failed to retrieve manifest from site.');
-      done();
-    });
-  });
-
-  it('Valid url but returning 404 should return an Error in callback.', function(done) {
-    responseFunction = function(req, res) {
-      res.writeHead(404);
-      res.end();
-    };
-
-    tools.getManifestFromSite('http://localhost:8042/notfound', function(err) {
-      should.exist(err);
-      err.should.have.property('message', 'Failed to retrieve manifest from site.');
-      done();
-    });
-  });
-
-  it('Should retrieve the manifest info object from a site that contains the manifest tag.');
-
-  it('Should create the manifest info object from a site that does not contains the manifest tag.');
-
-  //responseFunction = function(req, res) {
-  //  var url_parts = url.parse(req.url);
-  //  var route = url_parts.pathname;
-  //});
-
-  afterEach(function () {
-    responseFunction = undefined;
-  });
-
-  after(function () {
-    server.close();
-  });
-});
-
-describe('convertTo()', function () {
-  it('Convert from W3C to chromeOS.');
-  it('Convert from W3C to W3C.');
-  it('Convert from chromeOS to W3C.');
-  it('Convert from invalid to W3C.');
-  it('Convert from W3C to invalid.');
 });
--- test/utils.js
+++ test/utils.js
@@ -3,22 +3,24 @@
 var utils = require('../lib/utils');
 var should = require('should');
 
-describe('parseJSON()', function () {
-  it('Return the parsed JSON.', function() {
-    var inputJSON = '{"key": "value", "number": 42 }';
-    var result = utils.parseJSON(inputJSON);
-    should.exist(result);
-    result.should.have.property('key', 'value');
-    result.should.have.property('number', 42);
-  });
+describe('utils', function () {
+  describe('parseJSON()', function () {
+    it('Return the parsed JSON.', function() {
+      var inputJSON = '{"key": "value", "number": 42 }';
+      var result = utils.parseJSON(inputJSON);
+      should.exist(result);
+      result.should.have.property('key', 'value');
+      result.should.have.property('number', 42);
+    });
 
-  it('undefined should return undefined.', function() {
-    var result = utils.parseJSON(undefined);
-    should.not.exist(result);
-  });
+    it('undefined should return undefined.', function() {
+      var result = utils.parseJSON(undefined);
+      should.not.exist(result);
+    });
 
-  it('String should return undefined.', function() {
-    var result = utils.parseJSON('this is a string');
-    should.not.exist(result);
+    it('String should return undefined.', function() {
+      var result = utils.parseJSON('this is a string');
+      should.not.exist(result);
+    });
   });
 });
