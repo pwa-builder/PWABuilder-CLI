b6b0992a9d8aae15b2113b1c9a22d02f9d48824f
Fernando Tubio <fernando.tubio@southworks.net>
2015-03-30 22:32:08 -0300

    Added rudimentary CLI to manifest-tools to enable retrieving its manifest URL and downloading the manifest. (WIP)

---
+++ manifest-tools.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+var tools = require('./lib/tools');
+
+var siteUrl = process.argv[2];
+console.log('Scanning ' + siteUrl + ' for manifest...');
+tools.getManifestFromSite(siteUrl, function (err, manifest) {
+    if (err) {
+        console.log(err);
+        return err;
+    }
+    
+
+    console.log('Found a ' + manifest.format.toUpperCase() + ' manifest');
+    console.log();
+    console.log(JSON.stringify(manifest.content, null, 4));
+});
