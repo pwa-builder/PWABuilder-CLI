e8f4890afdae93f28c004713f0056f0fa656e04a
mrodriguez <marcelo.rodriguez@southworks.net>
2015-04-16 17:54:07 -0300

    Added run command to launch the cordova apps for windows and android

--- manifoldjs.js
+++ manifoldjs.js
@@ -4,36 +4,53 @@ var validations = require('./lib/common/validations'),
     constants = require('./lib/manifestTools/constants'),
     manifestTools = require('./lib/manifestTools'),
     projectBuilder = require('./lib/projectBuilder'),
-    validationConstants = require('./lib/manifestTools/validationConstants');
+    projectTools = require('./lib/projectTools'),
+    validationConstants = require('./lib/manifestTools/validationConstants'),
     url = require('url'),
     log = require('loglevel');
 
 
 function checkParameters(argv) {
-    if (argv._.length < 1) {
-        throw 'ERROR: Missing required <website-url> parameter.';
-    } else if (argv._.length > 1) {
-        throw 'ERROR: Unexpected parameters.';
+  if (argv._.length < 1) {
+    throw 'ERROR: Missing required parameters.';
+  } else if(argv._.length === 1) {
+    if (argv._[0].toLowerCase() === 'run') {
+      throw 'ERROR: Missing platform parameter.';
     }
-
-    // check platforms
-    if (argv.platforms) {
-        var platforms = argv.platforms.split(/[\s,]+/);
-        if (!validations.platformsValid(platforms)) {
-            throw 'ERROR: Invalid platform(s) specified.';
-        }
+  } else if (argv._.length === 2) {
+    if (argv._[0].toLowerCase() !== 'run') {
+      throw 'ERROR: Invalid parameters.';
+    } else {
+      if (!validations.platformToRunValid(argv._[1])) {
+        throw 'ERROR: Invalid platform specified.';
+      }
     }
-
-    // check log level
-    if (argv.loglevel) {
-        if (!validations.logLevelValid(argv.loglevel)) {
-            throw 'ERROR: Invalid loglevel specified. Valid values are: debug, trace, info, warn, error';
-        }
+  } else {
+    throw 'ERROR: Unexpected parameters.';
+  }
+  
+  // check platforms
+  if (argv.platforms) {
+    var platforms = argv.platforms.split(/[\s,]+/);
+    if (!validations.platformsValid(platforms)) {
+      throw 'ERROR: Invalid platform(s) specified.';
     }
+  }
+  
+  // check log level
+  if (argv.loglevel) {
+    if (!validations.logLevelValid(argv.loglevel)) {
+      throw 'ERROR: Invalid loglevel specified. Valid values are: debug, trace, info, warn, error';
+    }
+  }
 }
 
 var parameters = require('optimist')
-                .usage('Usage: node appmyweb <website-url> [-d <app-directory>] [-s <short-name>] [-p <platforms>] [-l <log-level>] [-b] [-m <manifest-file>]')
+                .usage('Usage: manifoldjs <website-url> [-d <app-directory>] [-s <short-name>]\n' +
+                       '                                [-p <platforms>] [-l <log-level>]\n' +
+                       '                                [-b] [-m <manifest-file>]\n' +
+                       '-or-\n' +
+                       '       manifoldjs run <windows|android>')
                 .alias('d', 'directory')
                 .alias('s', 'shortname')
                 .alias('p', 'platforms')
@@ -48,25 +65,40 @@ var parameters = require('optimist')
                 .check(checkParameters)
                 .argv;
 
-var siteUrl = parameters._[0];
-var rootDir = parameters.directory ? parameters.directory : process.cwd();
-var platforms = parameters.platforms.split(/[\s,]+/);
-
 global.logLevel = parameters.loglevel;
 log.setLevel(global.logLevel);
 
-manifestTools.getW3cManifest(siteUrl, parameters.manifest, function (err, manifestInfo) {
+if (parameters._[0].toLowerCase() === "run") {
+  // Run the cordova app for the specified platform
+  
+  var platform = parameters._[1];
+  projectTools.runCordovaApp(platform, function (err) {
     if (err) {
-        log.error('ERROR: ' + err.message);
-        return;
+      log.error('ERROR: ' + err.message);
+      return;
     }
+    
+    log.info('The application was launched successfully!');
+  });
 
+} else {
+  // Create the apps for the specified platforms
+
+  var siteUrl = parameters._[0];
+  var rootDir = parameters.directory ? parameters.directory : process.cwd();
+  var platforms = parameters.platforms.split(/[\s,]+/);
+  manifestTools.getW3cManifest(siteUrl, parameters.manifest, function (err, manifestInfo) {
+    if (err) {
+      log.error('ERROR: ' + err.message);
+      return;
+    }
+    
     manifestTools.validateManifest(manifestInfo, platforms, function (err, validationResults) {
       if (err) {
         log.warn('ERROR: ' + err.message);
         return;
       }
-
+      
       validationResults.forEach(function (validationResult) {
         var validationMessage = 'Validation ' + validationResult.level + ' (' + validationResult.platform + '): ' + validationResult.description;
         if (validationResult.level === validationConstants.levels.warning) {
@@ -75,23 +107,24 @@ manifestTools.getW3cManifest(siteUrl, parameters.manifest, function (err, manife
           log.info(validationMessage);
         }
       });
-
+      
       // if specified as a parameter, override the app's short name
       if (parameters.s) {
         manifestInfo.content.short_name = parameters.s;
       }
-
+      
       log.debug('Manifest contents:');
       log.debug(JSON.stringify(manifestInfo.content, null, 4));
-
+      
       // create the cordova application
       projectBuilder.createApps(manifestInfo, rootDir, platforms, parameters.build, function (err) {
         if (err) {
           log.warn('WARNING: ' + err.message);
           return;
         }
-
+        
         log.info('The application(s) are ready!');
       });
     });
-});
+  });
+}
