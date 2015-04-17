ce78c85010f4c00691cde6d521fcf77552fc02a2
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2015-04-17 17:57:18 -0300

    [validations] Added unit test for validations targeting all platforms

---
+++ test/manifestTools/validations/all/requiredAccessRules.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var validation = require('../../../../lib/manifestTools/validationRules/all/requiredAccessRules');
+var validationConstants = require('../../../../lib/manifestTools/validationConstants');
+var should = require('should');
+
+describe('Validation - All', function () {
+  describe('requiredAccessRules', function () {
+    it('Should return a suggestion if manifest does not contains access rules', function(done) {
+      validation({}, function(err, suggestion) {
+        should.not.exist(err);
+        should.exist(suggestion);
+        suggestion.should.have.property('platform', validationConstants.platforms.all);
+        suggestion.should.have.property('level', validationConstants.levels.suggestion);
+        suggestion.should.have.property('member', validationConstants.manifestMembers.hap_urlAccess);
+        suggestion.should.have.property('code', validationConstants.codes.requiredValue);
+        done();
+      });
+    });
+
+    it('Should return a suggestion if manifest access rules is not an array', function(done) {
+      validation({ hap_urlAccess: 'test' }, function(err, suggestion) {
+        should.not.exist(err);
+        should.exist(suggestion);
+        suggestion.should.have.property('platform', validationConstants.platforms.all);
+        suggestion.should.have.property('level', validationConstants.levels.suggestion);
+        suggestion.should.have.property('member', validationConstants.manifestMembers.hap_urlAccess);
+        suggestion.should.have.property('code', validationConstants.codes.requiredValue);
+        done();
+      });
+    });
+
+    it('Should return a suggestion if manifest access rules is empty', function(done) {
+      validation({ hap_urlAccess: [] }, function(err, suggestion) {
+        should.not.exist(err);
+        should.exist(suggestion);
+        suggestion.should.have.property('platform', validationConstants.platforms.all);
+        suggestion.should.have.property('level', validationConstants.levels.suggestion);
+        suggestion.should.have.property('member', validationConstants.manifestMembers.hap_urlAccess);
+        suggestion.should.have.property('code', validationConstants.codes.requiredValue);
+        done();
+      });
+    });
+
+    it('Should not return a suggestion if manifest access rules is not empty', function(done) {
+      validation({ hap_urlAccess: ['something'] }, function(err, suggestion) {
+        should.not.exist(err);
+        should.not.exist(suggestion);
+        done();
+      });
+    });
+  });
+});
