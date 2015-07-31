9c0bd43469062f63ca1c765e4c66dfd58dda52c6
Nahuel Beni <nahuel.beni@gmail.com>
2015-07-31 11:58:42 -0300

    fixes due to failing tests and jshint errors

--- manifoldjs.js
+++ manifoldjs.js
@@ -165,7 +165,7 @@ if (program.args[0] && program.args[0].toLowerCase() === 'run') {
     log.debug(JSON.stringify(manifestInfo.content, null, 4));
 
     // add generatedFrom value to manifestInfo for telemetry
-    manifestInfo.generatedFrom = "CLI";
+    manifestInfo.generatedFrom = 'CLI';
 
     // Create the apps for the specified platforms
     projectBuilder.createApps(manifestInfo, rootDir, platforms, program, function (err) {
