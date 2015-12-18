15c3af5dd9ede69a16f71479f56c444702914b3d
Fernando Tubio <fernando.tubio@southworks.net>
2015-12-18 01:25:43 -0300

    Updated code to report manifest validation errors

--- manifoldjs-lib/src/projectBuilder.js
+++ manifoldjs-lib/src/projectBuilder.js
@@ -712,13 +712,7 @@ var createCordovaApp = function (w3cManifestInfo, generatedAppDir, platforms, op
   return task.promise;
 };
 
-// START - replacement
 var loadedPlatforms;
-
-var logResult = {};
-logResult[validationConstants.levels.suggestion] = log.info;
-logResult[validationConstants.levels.warning] = log.warn;
-logResult[validationConstants.levels.error] = log.error;
 
 var createApps = function (w3cManifestInfo, rootDir, platforms, options, callback) {
 
@@ -736,8 +730,13 @@ var createApps = function (w3cManifestInfo, rootDir, platforms, options, callbac
 	// output validation results
 	.then(function (validationResults) {
 		validationResults.forEach(function (result) {			
-			// logResult[result.level]('Manifest validation - ' + result.description, result.platform === constants.validation.platforms.all ? undefined : result.platform);
-      logResult[result.level]('Manifest validation - ' + result.description, result.platform);
+      if (result.level === validationConstants.levels.suggestion) {
+        log.info('Manifest validation - ' + result.description + ' (member: ' + result.member + ').', result.platform);        
+      } else if (result.level === validationConstants.levels.warning) {
+        log.warn('Manifest validation - ' + result.description + ' (member: ' + result.member + ').', result.platform);        
+      } else if (result.level === validationConstants.levels.error) {
+        log.error('Manifest validation - ' + result.description + ' (member: ' + result.member + ').', result.platform);        
+      }
 		});
 	})
 	.then(function () {
@@ -766,108 +765,107 @@ var createApps = function (w3cManifestInfo, rootDir, platforms, options, callbac
 		log.info('All done!');
 	});
 }
-// END - replacement
 
-var createAppsX = function (w3cManifestInfo, rootDir, platforms, options, callback) {
-  var errmsg = 'One or more errors occurred when generating the application.';
-  manifestTools.validateManifest(w3cManifestInfo, platforms, function (err, validationResults) {
-    if (err) {
-      log.error('ERROR: ' + err.message);
-      return callback && callback(wrapError(errmsg, err));
-    }
-
-    var invalidManifest = false;
-    validationResults.forEach(function (validationResult) {
-      var validationMessage = validationResult.level.toUpperCase() +  ': Manifest validation ' +  ' (' + validationConstants.platformDisplayNames[validationResult.platform] + ') - ' + validationResult.description + '[' + validationResult.member + ']';
-      if (validationResult.level === validationConstants.levels.warning) {
-        log.warn(validationMessage);
-      } else if (validationResult.level === validationConstants.levels.suggestion) {
-        log.warn(validationMessage);
-      } else if (validationResult.level === validationConstants.levels.error) {
-        log.error(validationMessage);
-        invalidManifest = true;
-      }
-    });
-
-    // report manifest validation errors
-    if (invalidManifest) {
-      var validationError = new Error(errmsg);
-      validationError.name = 'ValidationError';
-      validationError.validationResults = validationResults;
-      return callback && callback(validationError);
-    }
-
-    // determine the path where the Cordova app will be created
-    options.appName = utils.sanitizeName(w3cManifestInfo.content.short_name);
-    var generatedAppDir = path.join(rootDir, options.appName);
-
-    // Add timestamp to manifest information for telemetry purposes only
-    w3cManifestInfo.timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\.[0-9]+/, ' ');
-
-    // create app directory
-    mkdirp(generatedAppDir, function (err) {
-      if (err && err.code !== 'EEXIST') {
-        return callback && callback(wrapError(errmsg + ' Failed to create the directory for the app: \'' + generatedAppDir + '\'.', err));
-      }
-
-      // process all requested platforms
-      var pendingTasks = [];
-      var cordovaPlatforms = [];
-      var addWindows10Task = false;
-      platforms.forEach(function (el) {
-        var platform = el.toUpperCase();
-        if (platform === 'CHROME') {
-          pendingTasks.push(createChromeApp(w3cManifestInfo, generatedAppDir, options));
-        } else if (platform === 'FIREFOX') {
-          pendingTasks.push(createFirefoxOSApp(w3cManifestInfo, generatedAppDir, options));
-        } else if (platform === 'WINDOWS10') {
-          addWindows10Task = true;
-        } else if (platform === 'WINDOWS') {
-          cordovaPlatforms.push(el);
-          addWindows10Task = true;
-        } else if (platform === 'WEB') {
-          pendingTasks.push(createWebApp(w3cManifestInfo, generatedAppDir, options));
-        } else if ( platform === 'IOS') {
-          cordovaPlatforms.push(el);
-        } else if ( platform === 'ANDROID') {
-          cordovaPlatforms.push(el);
-        } else {
-          log.warn('WARNING: Ignoring unknown platform: \'' + el + '\'.');
-        }
-      });
-
-      // generate Cordova project
-      if (cordovaPlatforms.length) {
-        pendingTasks.push(createCordovaApp(w3cManifestInfo, generatedAppDir, cordovaPlatforms, options));
-      }
-
-      // generate windows 10 hosted web app
-      if (addWindows10Task) {
-        pendingTasks.push(createWindows10App(w3cManifestInfo, generatedAppDir, options));
-      }
-
-      Q.allSettled(pendingTasks)
-      .done(function (results) {
-        // report any errors
-        var innerErrors = [];
-        results.forEach(function (task) {
-          if (task.state !== 'fulfilled') {
-            log.error(task.reason.message);
-            if (task.reason.innerErrors) {
-              task.reason.innerErrors.forEach(function (innerError) {
-                log.debug(innerError.message);
-              });
-            }
-
-            innerErrors.push(task.reason);
-          }
-        });
-
-        return callback && callback(innerErrors.length ? wrapError(errmsg, innerErrors) : undefined);
-      });
-    });
-  });
-};
+// var createApps = function (w3cManifestInfo, rootDir, platforms, options, callback) {
+//   var errmsg = 'One or more errors occurred when generating the application.';
+//   manifestTools.validateManifest(w3cManifestInfo, platforms, function (err, validationResults) {
+//     if (err) {
+//       log.error('ERROR: ' + err.message);
+//       return callback && callback(wrapError(errmsg, err));
+//     }
+// 
+//     var invalidManifest = false;
+//     validationResults.forEach(function (validationResult) {
+//       var validationMessage = validationResult.level.toUpperCase() +  ': Manifest validation ' +  ' (' + validationConstants.platformDisplayNames[validationResult.platform] + ') - ' + validationResult.description + '[' + validationResult.member + ']';
+//       if (validationResult.level === validationConstants.levels.warning) {
+//         log.warn(validationMessage);
+//       } else if (validationResult.level === validationConstants.levels.suggestion) {
+//         log.warn(validationMessage);
+//       } else if (validationResult.level === validationConstants.levels.error) {
+//         log.error(validationMessage);
+//         invalidManifest = true;
+//       }
+//     });
+// 
+//     // report manifest validation errors
+//     if (invalidManifest) {
+//       var validationError = new Error(errmsg);
+//       validationError.name = 'ValidationError';
+//       validationError.validationResults = validationResults;
+//       return callback && callback(validationError);
+//     }
+// 
+//     // determine the path where the Cordova app will be created
+//     options.appName = utils.sanitizeName(w3cManifestInfo.content.short_name);
+//     var generatedAppDir = path.join(rootDir, options.appName);
+// 
+//     // Add timestamp to manifest information for telemetry purposes only
+//     w3cManifestInfo.timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\.[0-9]+/, ' ');
+// 
+//     // create app directory
+//     mkdirp(generatedAppDir, function (err) {
+//       if (err && err.code !== 'EEXIST') {
+//         return callback && callback(wrapError(errmsg + ' Failed to create the directory for the app: \'' + generatedAppDir + '\'.', err));
+//       }
+// 
+//       // process all requested platforms
+//       var pendingTasks = [];
+//       var cordovaPlatforms = [];
+//       var addWindows10Task = false;
+//       platforms.forEach(function (el) {
+//         var platform = el.toUpperCase();
+//         if (platform === 'CHROME') {
+//           pendingTasks.push(createChromeApp(w3cManifestInfo, generatedAppDir, options));
+//         } else if (platform === 'FIREFOX') {
+//           pendingTasks.push(createFirefoxOSApp(w3cManifestInfo, generatedAppDir, options));
+//         } else if (platform === 'WINDOWS10') {
+//           addWindows10Task = true;
+//         } else if (platform === 'WINDOWS') {
+//           cordovaPlatforms.push(el);
+//           addWindows10Task = true;
+//         } else if (platform === 'WEB') {
+//           pendingTasks.push(createWebApp(w3cManifestInfo, generatedAppDir, options));
+//         } else if ( platform === 'IOS') {
+//           cordovaPlatforms.push(el);
+//         } else if ( platform === 'ANDROID') {
+//           cordovaPlatforms.push(el);
+//         } else {
+//           log.warn('WARNING: Ignoring unknown platform: \'' + el + '\'.');
+//         }
+//       });
+// 
+//       // generate Cordova project
+//       if (cordovaPlatforms.length) {
+//         pendingTasks.push(createCordovaApp(w3cManifestInfo, generatedAppDir, cordovaPlatforms, options));
+//       }
+// 
+//       // generate windows 10 hosted web app
+//       if (addWindows10Task) {
+//         pendingTasks.push(createWindows10App(w3cManifestInfo, generatedAppDir, options));
+//       }
+// 
+//       Q.allSettled(pendingTasks)
+//       .done(function (results) {
+//         // report any errors
+//         var innerErrors = [];
+//         results.forEach(function (task) {
+//           if (task.state !== 'fulfilled') {
+//             log.error(task.reason.message);
+//             if (task.reason.innerErrors) {
+//               task.reason.innerErrors.forEach(function (innerError) {
+//                 log.debug(innerError.message);
+//               });
+//             }
+// 
+//             innerErrors.push(task.reason);
+//           }
+//         });
+// 
+//         return callback && callback(innerErrors.length ? wrapError(errmsg, innerErrors) : undefined);
+//       });
+//     });
+//   });
+// };
 
 module.exports = {
   createApps: createApps
