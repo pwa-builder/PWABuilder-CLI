668169999de78eb7566625eef88b3edcc565250a
Fernando Tubio <fernando.tubio@southworks.net>
2015-12-17 23:48:52 -0300

    Merged constants and validationConstants modules

--- manifoldjs-lib/src/constants.js
+++ manifoldjs-lib/src/constants.js
@@ -1,8 +1,42 @@
 'use strict';
 
+var validationConstants = {
+  levels: {
+    error: 'error',
+    warning: 'warning',
+    suggestion: 'suggestion'
+  },
+  codes: {
+    requiredValue: 'required-value',
+    invalidValue: 'invalid-value',
+    missingImageGroup: 'missing-image-group',
+    missingImage: 'missing-image',
+    missingImageOnsite: 'missing-image-onsite',
+    requiredAbsoluteUrl: 'requiredAbsoluteUrl',
+    deprecatedMember: 'deprecatedMember'
+  },
+  manifestMembers: {
+    lang: 'lang',
+    name: 'name',
+    short_name: 'short_name',
+    scope: 'scope',
+    icons: 'icons',
+    display: 'display',
+    orientation: 'orientation',
+    start_url: 'start_url',
+    theme_color: 'theme_color',
+    related_applications: 'related_applications',
+    prefer_related_applications: 'prefer_related_applications',
+    mjs_access_whitelist: 'mjs_access_whitelist',
+    mjs_api_access: 'mjs_api_acces',
+    mjs_extended_scope: 'mjs_extended_scope'
+  },
+  platforms: {
+    all: 'All Platforms'
+  }
+};
+
 module.exports = {
   BASE_MANIFEST_FORMAT: 'w3c',
-  CHROME_MANIFEST_FORMAT: 'chromeos',
-  FIREFOX_MANIFEST_FORMAT: 'firefox',
-  WINDOWS10_MANIFEST_FORMAT: 'windows10'
+  validation: validationConstants
 };
