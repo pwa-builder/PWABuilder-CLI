084a58d74412a228fa4d6f79ecde39084ec83e35
Fernando Tubio <fernando.tubio@southworks.net>
2015-12-17 23:52:20 -0300

    Exposed additional modules from manifoldjs-lib: CustomError, constants, fileTools, iconTools

--- manifoldjs-lib/index.js
+++ manifoldjs-lib/index.js
@@ -2,6 +2,8 @@
 
 module.exports = {
   PlatformBase: require('./src/platformBase'),
+  CustomError: require('./src/customError'),
+  constants: require('./src/constants'),
   utils: require('./src/utils'),
   validations: require('./src/validations'),
   manifestTools: require('./src/manifestTools'),
@@ -10,5 +12,7 @@ module.exports = {
   platformTools: require('./src/platformTools'),
   platformUtils: require('./src/platformUtils'),
   packageTools: require('./src/packageTools'),
+  fileTools: require('./src/fileTools'),
+  iconTools: require('./src/iconTools'),
   log: require('./src/log')
 };
