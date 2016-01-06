e8c3e94694eefdcbe724d0f1e78f39b31e235c45
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-06 19:06:40 -0300

    Relocated Windows 10 code and assets to platform project

--- manifoldjs-lib/src/projectBuilder.js
+++ manifoldjs-lib/src/projectBuilder.js
@@ -419,126 +419,126 @@ var updateProjectFiles = function (sourceDir, w3cManifestInfo, callback) {
     callback);
 };
 
-var createWindows10App = function (w3cManifestInfo, generatedAppDir /*, options*/) {
-  log.info('Generating the Windows 10 application...');
-
-  var task = Q.defer();
-
-  manifestTools.convertTo(w3cManifestInfo, 'windows10', function (err, windowsManifestInfo) {
-    if (err) {
-      return task.reject(wrapError('ERROR: Failed to convert the Windows 10 manifest. The Windows 10 project could not be created successfully.', err));
-    }
-
-    var platformDir = path.join(generatedAppDir, 'windows10');
-    var manifestDir = path.join(platformDir, 'manifest');
-    var manifestImagesDir = path.join(manifestDir, 'images');
-    var sourceDir = path.join(platformDir, 'source');
-    var sourceImagesDir = path.join(sourceDir, 'images');
-    var projectAssetsDir = path.join(__dirname, 'projectBuilder', 'assets', 'windows10');
-    var projectAssetsImagesDir = path.join(projectAssetsDir, 'images');
-
-    // if the manifest images folder doesn't exist, create it
-    mkdirp(manifestImagesDir, function (err) {
-      if (err && err.code !== 'EEXIST') {
-        return task.reject(wrapError('ERROR: Failed to create the platform directory. The Windows 10 project could not be created successfully.', err));
-      }
-
-      // download icons to the app's folder
-      var pendingDownloads = [];
-      log.info('Downloading Windows 10 icons...');
-      var icons = windowsManifestInfo.content.icons;
-
-      function downloadIcon(size) {
-        var downloadTask = new Q.defer();
-        pendingDownloads.push(downloadTask.promise);
-        var iconUrl = url.resolve(w3cManifestInfo.content.start_url, icons[size].url);
-        var iconFilePath = path.join(manifestImagesDir, icons[size].fileName);
-        downloader.downloadImage(iconUrl, iconFilePath, createDownloaderImageCallback(downloadTask, icons, size));
-      }
-
-      function createDownloaderImageCallback(downloadTask, icons, size) {
-        return function (err) {
-          if (err) {
-            log.warn('WARNING: Failed to download icon file: ' + icons[size].url);
-            log.debug(err.message);
-          }
-
-          downloadTask.resolve();
-        };
-      }
-
-      for (var size in icons) {
-        downloadIcon(size);
-      }
-
-      Q.allSettled(pendingDownloads)
-      .done(function () {
-        // copy the manifest file to the app folder
-        log.info('Copying the Windows 10 manifest to the app folder...');
-        var manifestFilePath = path.join(manifestDir, 'appxmanifest.xml');
-        fs.writeFile(manifestFilePath, windowsManifestInfo.content.rawData, function (err) {
-          if (err) {
-            return task.reject(wrapError('ERROR: Failed to copy the manifest to the platform folder. The Windows 10 project could not be created successfully.', err));
-          }
-
-          // If there are missing images in the manifest folder, copy the default images from the project assets
-          ncp(projectAssetsImagesDir, manifestImagesDir, { clobber: false }, function (err) {
-            if (err) {
-              return task.reject(wrapError('ERROR: Failed to copy the default icons to the project folder. The Windows 10 project could not be created successfully.', err));
-            }
-
-            // copy project assets to the source folder
-            ncp(projectAssetsDir, sourceDir, function (err) {
-              if (err) {
-                return task.reject(wrapError('ERROR: Failed to copy the project assets to the source folder. The Windows 10 project could not be created successfully.', err));
-              }
-
-              // Overwrite images in source folder with the manifest images
-              ncp(manifestImagesDir, sourceImagesDir, { clobber: true }, function (err) {
-                if (err) {
-                  return task.reject(wrapError('ERROR: Failed to copy the manifest icons to the project folder. The Windows 10 project could not be created successfully.', err));
-                }
-
-                // Update project files in source dir
-                updateProjectFiles(sourceDir, w3cManifestInfo, function (err) {
-                  if (err) {
-                    return task.reject(wrapError('ERROR: Failed to update Windows 10 package.appxmanifest. The Windows 10 project files could not be created successfully.', err));
-                  }
-
-
- 
-                // move offline file into win10 dir
-                copyOfflineFile('msapp-error.html', manifestDir, function (err) {
-                  if (err) {
-                    return task.reject(wrapError('ERROR: Failed to update Windows 10 package with offline file. The Windows 10 project files could not be created successfully.', err));
-                  }
-                // move offline file into win10 source dir
-                copyOfflineFile('msapp-error.html', sourceDir, function (err) {
-                  if (err) {
-                    return task.reject(wrapError('ERROR: Failed to update Windows 10 package with offline file. The Windows 10 project files could not be created successfully.', err));
-                  }
-
-                  copyDocFile('Windows10-next-steps.md', platformDir, function (err) {
-                    if (err) {
-                      log.warn('WARNING: Failed to copy documentation file to the Windows 10 platform folder.');
-                      log.debug(err.message);
-                    }
-                    
-                    return task.resolve();
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-
-  return task.promise;
-};
+// var createWindows10App = function (w3cManifestInfo, generatedAppDir /*, options*/) {
+//   log.info('Generating the Windows 10 application...');
+// 
+//   var task = Q.defer();
+// 
+//   manifestTools.convertTo(w3cManifestInfo, 'windows10', function (err, windowsManifestInfo) {
+//     if (err) {
+//       return task.reject(wrapError('ERROR: Failed to convert the Windows 10 manifest. The Windows 10 project could not be created successfully.', err));
+//     }
+// 
+//     var platformDir = path.join(generatedAppDir, 'windows10');
+//     var manifestDir = path.join(platformDir, 'manifest');
+//     var manifestImagesDir = path.join(manifestDir, 'images');
+//     var sourceDir = path.join(platformDir, 'source');
+//     var sourceImagesDir = path.join(sourceDir, 'images');
+//     var projectAssetsDir = path.join(__dirname, 'projectBuilder', 'assets', 'windows10');
+//     var projectAssetsImagesDir = path.join(projectAssetsDir, 'images');
+// 
+//     // if the manifest images folder doesn't exist, create it
+//     mkdirp(manifestImagesDir, function (err) {
+//       if (err && err.code !== 'EEXIST') {
+//         return task.reject(wrapError('ERROR: Failed to create the platform directory. The Windows 10 project could not be created successfully.', err));
+//       }
+// 
+//       // download icons to the app's folder
+//       var pendingDownloads = [];
+//       log.info('Downloading Windows 10 icons...');
+//       var icons = windowsManifestInfo.content.icons;
+// 
+//       function downloadIcon(size) {
+//         var downloadTask = new Q.defer();
+//         pendingDownloads.push(downloadTask.promise);
+//         var iconUrl = url.resolve(w3cManifestInfo.content.start_url, icons[size].url);
+//         var iconFilePath = path.join(manifestImagesDir, icons[size].fileName);
+//         downloader.downloadImage(iconUrl, iconFilePath, createDownloaderImageCallback(downloadTask, icons, size));
+//       }
+// 
+//       function createDownloaderImageCallback(downloadTask, icons, size) {
+//         return function (err) {
+//           if (err) {
+//             log.warn('WARNING: Failed to download icon file: ' + icons[size].url);
+//             log.debug(err.message);
+//           }
+// 
+//           downloadTask.resolve();
+//         };
+//       }
+// 
+//       for (var size in icons) {
+//         downloadIcon(size);
+//       }
+// 
+//       Q.allSettled(pendingDownloads)
+//       .done(function () {
+//         // copy the manifest file to the app folder
+//         log.info('Copying the Windows 10 manifest to the app folder...');
+//         var manifestFilePath = path.join(manifestDir, 'appxmanifest.xml');
+//         fs.writeFile(manifestFilePath, windowsManifestInfo.content.rawData, function (err) {
+//           if (err) {
+//             return task.reject(wrapError('ERROR: Failed to copy the manifest to the platform folder. The Windows 10 project could not be created successfully.', err));
+//           }
+// 
+//           // If there are missing images in the manifest folder, copy the default images from the project assets
+//           ncp(projectAssetsImagesDir, manifestImagesDir, { clobber: false }, function (err) {
+//             if (err) {
+//               return task.reject(wrapError('ERROR: Failed to copy the default icons to the project folder. The Windows 10 project could not be created successfully.', err));
+//             }
+// 
+//             // copy project assets to the source folder
+//             ncp(projectAssetsDir, sourceDir, function (err) {
+//               if (err) {
+//                 return task.reject(wrapError('ERROR: Failed to copy the project assets to the source folder. The Windows 10 project could not be created successfully.', err));
+//               }
+// 
+//               // Overwrite images in source folder with the manifest images
+//               ncp(manifestImagesDir, sourceImagesDir, { clobber: true }, function (err) {
+//                 if (err) {
+//                   return task.reject(wrapError('ERROR: Failed to copy the manifest icons to the project folder. The Windows 10 project could not be created successfully.', err));
+//                 }
+// 
+//                 // Update project files in source dir
+//                 updateProjectFiles(sourceDir, w3cManifestInfo, function (err) {
+//                   if (err) {
+//                     return task.reject(wrapError('ERROR: Failed to update Windows 10 package.appxmanifest. The Windows 10 project files could not be created successfully.', err));
+//                   }
+// 
+// 
+//  
+//                 // move offline file into win10 dir
+//                 copyOfflineFile('msapp-error.html', manifestDir, function (err) {
+//                   if (err) {
+//                     return task.reject(wrapError('ERROR: Failed to update Windows 10 package with offline file. The Windows 10 project files could not be created successfully.', err));
+//                   }
+//                 // move offline file into win10 source dir
+//                 copyOfflineFile('msapp-error.html', sourceDir, function (err) {
+//                   if (err) {
+//                     return task.reject(wrapError('ERROR: Failed to update Windows 10 package with offline file. The Windows 10 project files could not be created successfully.', err));
+//                   }
+// 
+//                   copyDocFile('Windows10-next-steps.md', platformDir, function (err) {
+//                     if (err) {
+//                       log.warn('WARNING: Failed to copy documentation file to the Windows 10 platform folder.');
+//                       log.debug(err.message);
+//                     }
+//                     
+//                     return task.resolve();
+//                       });
+//                     });
+//                   });
+//                 });
+//               });
+//             });
+//           });
+//         });
+//       });
+//     });
+//   });
+// 
+//   return task.promise;
+// };
 
 // var createCordovaApp = function (w3cManifestInfo, generatedAppDir, platforms, options) {
 //   log.info('Generating the Cordova application...');
