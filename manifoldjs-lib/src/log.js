ea3c4ef5a868a80bc472421b603f08661ad487a5
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-06 17:14:20 -0300

    Changed log module to singleton to prevent individual platform packages from loading different instances depending on how they reference manifoldjs-lib

--- manifoldjs-lib/src/log.js
+++ manifoldjs-lib/src/log.js
@@ -1,27 +1,33 @@
 'use strict';
 
-var log = require('loglevel'),
-		path = require('path');
+var log = global.manifoldjs_logger, 
+    path = require('path');
+    
+if (!log) {
+  log = require('loglevel');
+  
+  var appPath = path.dirname(require.main.filename);
+  var app = require(path.join(appPath, 'package.json'));
 
-var appPath = path.dirname(require.main.filename);
-var app = require(path.join(appPath, 'package.json'));
+  var maxLenSeverity = 5;
+  var maxLenSource = 12;
 
-var maxLenSeverity = 5;
-var maxLenSource = 12;
+  var originalFactory = log.methodFactory;
+  log.methodFactory = function (methodName, logLevel, loggerName) {
+    var rawMethod = originalFactory(methodName, logLevel, loggerName);
 
-var originalFactory = log.methodFactory;
-log.methodFactory = function (methodName, logLevel, loggerName) {
-	var rawMethod = originalFactory(methodName, logLevel, loggerName);
-
-	return function (message, source, severity) {
-		message = message.replace(/\n/g, '\n' + Array(maxLenSeverity + maxLenSource + 6).join(' '));
-		source = source || loggerName || app.name;
-		severity = severity || methodName;
-		rawMethod(
-			'[' + severity + Array(Math.max(maxLenSeverity - severity.length + 1, 0)).join(' ') + '] '
-			+ source + Array(Math.max(maxLenSource - source.length + 1, 0)).join(' ') + ': '
-			+ message);
-	};
+    return function (message, source, severity) {
+      message = message.replace(/\n/g, '\n' + Array(maxLenSeverity + maxLenSource + 6).join(' '));
+      source = source || loggerName || app.name;
+      severity = severity || methodName;
+      rawMethod(
+        '[' + severity + Array(Math.max(maxLenSeverity - severity.length + 1, 0)).join(' ') + '] '
+        + source + Array(Math.max(maxLenSource - source.length + 1, 0)).join(' ') + ': '
+        + message);
+    };
+  }
+  
+  global.manifoldjs_logger = log;
 }
 
 module.exports = log;
