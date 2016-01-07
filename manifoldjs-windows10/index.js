3deca4e7e3eba4650382bb271349160fa83d0d62
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-07 00:03:21 -0300

    Restored Platform export in Windows 10 folder

--- manifoldjs-windows10/index.js
+++ manifoldjs-windows10/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
 module.exports = {
-  // Platform: require('./lib/platform'),
-  // TEMPORARY EXPORTS - WILL BE REMOVED
-  windows10Utils: require('./lib/windows10Utils')
+  Platform: require('./lib/platform'),
 };
-
