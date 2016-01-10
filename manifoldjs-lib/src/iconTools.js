ecc7c8d1280ddf84070b682ddc6274434dc97905
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-10 15:05:24 -0300

    Updated handling of downloaded files in getIcon function - manages errors and verifies content type

--- manifoldjs-lib/src/iconTools.js
+++ manifoldjs-lib/src/iconTools.js
@@ -13,12 +13,17 @@ function getIcon(iconUrl, iconFilePath, callback) {
 
 	var iconFolder = path.dirname(iconFilePath);
 
-	log.debug('Downloading icon file from [' + iconUrl + '] to [' + iconFilePath + ']');
+	log.debug('Downloading icon file from \'' + iconUrl + '\' to \'' + iconFilePath + '\'...');
 	return fileTools.mkdirp(iconFolder)
 		.then(function () {
 			return download(iconUrl, iconFilePath)
-				.then(function () {
-					return Q.resolve(iconFilePath);
+				.then(function (response) {
+          if (response.statusCode === 200 && !response.contentType.match(/image/)) {
+            // If not an image, return an error
+            return Q.reject(new Error('Downloaded file  \'' + iconUrl + '\' does not appear to be an image. The content type of the response was \'' + response.contentType + '\'.'));
+          }
+          
+					return iconFilePath;
 				})
 		})
 		.nodeify(callback);
