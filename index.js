5128b97bd500b189878f4d3ea86274747496bcd6
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-13 02:13:36 -0300

    Support 'legacy' API in CLI project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 ﻿/**
- * the top-level API for `manifoldjs`. provides access to the key libraries in
+ * **NOTE**: Use of manifoldjs as a library is now DEPRECATED. Use manifoldjs-lib instead.
+ *
+ * The top-level API for `manifoldjs`. Provides access to the key libraries in
  * manifoldjs so you can write your own tools using `manifoldjs` as a library.
  *
  * Usage
@@ -9,9 +11,35 @@
  *
  */
 
+var lib = require('manifoldjs-lib');
+var libValidationContants = lib.constants.validation;
+
+// Maintain compatibility with original constant definitions
+var validationConstants = {
+  levels:           libValidationContants.levels,
+  codes:            libValidationContants.codes,
+  manifestMembers:  libValidationContants.manifestMembers,
+  platforms: {
+    all: 'all',
+    android: 'android',
+    chrome: 'chrome',
+    firefox: 'firefox',
+    ios: 'ios',
+    windows: 'windows'
+  },
+  platformDisplayNames: {
+    all: 'All Platforms',
+    android: 'Android',
+    chrome: 'Chrome',
+    firefox: 'Firefox',
+    ios: 'iOS',
+    windows: 'Windows'
+  }
+};
+
 module.exports = {
-  manifestTools: require('./lib/manifestTools'),
-  validationConstants : require('./lib/manifestTools/validationConstants'),
-  projectBuilder: require('./lib/projectBuilder'),
-  projectTools: require('./lib/projectTools.js')
+  manifestTools:        lib.manifestTools,
+  validationConstants:  validationConstants,
+  projectBuilder:       lib.projectBuilder,
+  projectTools:         lib.projectTools
 };
