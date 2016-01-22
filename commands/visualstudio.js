18197217bdbbdd9197e2cfcc3e0739186104b70e
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-22 01:21:37 -0300

    Restore original behavior of visualstudio command, but flag as deprecated

--- commands/visualstudio.js
+++ commands/visualstudio.js
@@ -1,11 +1,53 @@
 'use strict';
 
+var Q = require('q');
+
 var lib = require('manifoldjs-lib');
 
-var log = lib.log;
+var CustomError = lib.CustomError,
+    exec = lib.processTools.exec,
+    fileTools = lib.fileTools,
+    log = lib.log;
 
-function run() {
-  log.warn('The \'visualstudio\' command is deprecated. Use \'manifoldjs open <windows|windows10>\' instead.')
+var open = require('./open');
+
+function isWindows10Version (version) {
+  return /^10/.test(version);
 }
 
-module.exports = run;
+function getWindowsVersion (callback) {
+  log.debug('Obtaining Windows version...');
+  exec('powershell', ['(Get-WmiObject win32_operatingsystem).version']).then(function (result) {
+    return result.stdout.trim();
+  })
+  .catch (function (err) {
+    return Q.reject(new CustomError('Failed to run the app for Windows platform.', err));    
+  })
+  .nodeify(callback); 
+}
+
+// implements the original behavior of the visualstudio command
+//  open windows10 project, if available, otherwise, open the windows project
+function runApp(program) {
+  
+  log.warn('The \'visualstudio\' command is deprecated. Use \'manifoldjs open <windows|windows10>\' instead.');
+  
+  var dir = process.cwd();
+  fileTools.searchFile(dir, 'App.jsproj', function (err, results) {
+    Q.ninvoke(getWindowsVersion).then(function (version) {
+      if (results && results.length > 0 && isWindows10Version(version)) {
+        program.args.push('windows10'); 
+        return open(program);
+      }
+      
+      fileTools.searchFile(dir, 'CordovaApp.sln', function (err, results) {
+        if (results && results.length > 0) {
+          program.args.push('windows');
+          return open(program); 
+        }
+      });
+    });
+  });
+}
+
+module.exports = runApp;
